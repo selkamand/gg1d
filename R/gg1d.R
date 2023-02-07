bb522d42937caff4638be981bd2f8d5fd3fae3b5
selkamand <sam.elkamand@gmail.com>
2023-02-08 01:49:36 +1100

    added options for changing text size and numeric ylab margin (distance to plot)

--- R/gg1d.R
+++ R/gg1d.R
@@ -180,6 +180,8 @@ choose_colours <- function(data, palettes, plottable, ndistinct, coltype, colour
 #' @param na_marker_size how large should the na_marker be (number)
 #' @param vertical_spacing how large should the gap between each data row be (unit = pt) (number)
 #' @param ignore_column_regex a regex string that, if matches a column name,  will cause that  column to be exclude from plotting (string)  (default: "_ignore$")
+#' @param fontsize_y_text size of y axis text (number)
+#' @param numeric_ylab_margin_right margin between numeric ylab and plot. remove (number)
 #'
 #' @return ggiraph interactive visualisation
 #'
@@ -207,7 +209,10 @@ gg1d_plot <- function(
     legend_title_beautify = TRUE,
     legend_nrow = 4, legend_ncol = NULL,
     legend_title_size = NULL, legend_text_size = NULL, legend_key_size = 0.3,
-    vertical_spacing = 0, na_marker = "!", na_marker_size = 4) {
+    vertical_spacing = 0, na_marker = "!", na_marker_size = 4,
+    fontsize_y_text = 12,
+    numeric_ylab_margin_right = -2
+    ) {
 
   # Assertions --------------------------------------------------------------
   assertions::assert_dataframe(.data)
@@ -379,7 +384,8 @@ gg1d_plot <- function(
             legend_title_size = legend_title_size,
             legend_text_size = legend_text_size,
             legend_key_size = legend_key_size,
-            vertical_spacing = vertical_spacing
+            vertical_spacing = vertical_spacing,
+            fontsize_y_text = fontsize_y_text
             ) +
           ggplot2::guides(fill = ggplot2::guide_legend(
             title.position = legend_title_position,
@@ -399,8 +405,10 @@ gg1d_plot <- function(
             ) +
           ggplot2::scale_x_discrete(drop = drop_unused_id_levels) +
           ggplot2::scale_y_continuous(breaks = sensible_2_breaks(.data[[colname]])) +
+          #ggplot2::annotate(geom = "text", x = -10 - nchar(colname)*8, label=colname, y = mean(sensible_2_breaks(.data[[colname]])))+
+          #ggplot2::coord_cartesian(ylim = range(.data[[colname]]), clip = TRUE) +
           ggplot2::ylab(if(legend_title_beautify) beautify(colname) else colname) +
-          theme_numeric(vertical_spacing = vertical_spacing)
+          theme_numeric(vertical_spacing = vertical_spacing, fontsize_y_text = fontsize_y_text, ylab_margin_right = numeric_ylab_margin_right)
       } else {
         cli::cli_abort("Unsure how to plot coltype: {coltype}")
       }
@@ -438,12 +446,12 @@ gg1d_plot <- function(
 }
 
 
-theme_categorical <- function(show_legend = TRUE, show_legend_titles = FALSE, legend_position = "right", legend_title_size = NULL, legend_text_size = NULL, legend_key_size = 0.3, vertical_spacing = 0) {
+theme_categorical <- function(fontsize_y_text = 12, show_legend = TRUE,show_legend_titles = FALSE, legend_position = "right", legend_title_size = NULL, legend_text_size = NULL, legend_key_size = 0.3, vertical_spacing = 0) {
   ggplot2::theme_minimal() %+replace%
 
     theme(
       panel.grid = element_blank(),
-      #axis.text.y.left = element_blank(),
+      axis.text.y.left = element_text(size = fontsize_y_text),
       axis.text.x = element_blank(),
       axis.title.x = element_blank(),
       axis.title.y = element_blank(),
@@ -460,14 +468,14 @@ theme_categorical <- function(show_legend = TRUE, show_legend_titles = FALSE, le
     )
 }
 
-theme_numeric <- function(vertical_spacing = 0) {
+theme_numeric <- function(vertical_spacing = 0, ylab_margin_right = -2, fontsize_y_text = 12) {
   ggplot2::theme_minimal() %+replace%
 
     theme(
       panel.grid = element_blank(),
       axis.title.y.left = element_text(
-        angle = 0, vjust = 0.5,
-        margin = ggplot2::margin(0, -2, 0, 0)
+        angle = 0, vjust = 0.5, size = fontsize_y_text,
+        margin = ggplot2::margin(0, -ylab_margin_right, 0, 0)
         ),
       axis.text.x = element_blank(),
       axis.title.x = element_blank(),

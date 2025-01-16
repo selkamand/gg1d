4b4c6b654c16c40384a3719f6f581de063da0fcc
Sam El-Kamand <sam.elkamand@gmail.com>
2025-01-16 18:00:21 +1100

    feat: added PCPs to EDA with minimal options

--- shiny/ggnd/server.R
+++ shiny/ggnd/server.R
@@ -4,8 +4,6 @@ library(ggiraph)
 server <- function(input, output, session) {
 
   ## Set Selector Choices based on column names
-
-
   dataset <- reactiveVal(value = iris)
 
   observeEvent(dataset() , {
@@ -19,11 +17,33 @@ server <- function(input, output, session) {
     updateSelectInput(
       session = session,
       inputId = "in_sel_sort_col",
-      choices = c("Column to Sort By"= "", colnames(dataset())),
+      choices = c("", colnames(dataset())),
       selected = "",
     )
 
+    updateSelectInput(
+      session = session,
+      inputId = "in_sel_pcp_col_colour",
+      choices = c("", colnames(dataset())),
+      selected = "",
+    )
   })
+
+  observeEvent(input$in_sel_pcp_col_colour, {
+    validate(need(!is.null(input$in_sel_pcp_col_colour) && nchar(input$in_sel_pcp_col_colour) > 0 , message = "Requires selection of Column to Colour by"))
+
+    updateSelectInput(
+      session = session,
+      inputId = "in_sel_pcp_highlight",
+      choices = c("Column to Sort By"= "", unique(dataset()[input$in_sel_pcp_col_colour])),
+      selected = "",
+    )
+  })
+
+  # Update Nav-Bar Page (showing options) based on Selected Plot Type
+  observeEvent( input$in_radio_plot_type, {
+    nav_select(id = "navset_visoptions", selected = input$in_radio_plot_type,session = session)
+  }, priority = 1)
 
 
 
@@ -48,6 +68,8 @@ server <- function(input, output, session) {
 
   output$out_plot_widget <- ggiraph::renderGirafe({
     data <- req(dataset())
+
+
 
     # Build gg1d_options from user input
     opts <- gg1d_options(
@@ -85,25 +107,40 @@ server <- function(input, output, session) {
     # Now call gg1d with user-specified parameters
     tryCatch(
       expr = {
-        gg1d(
-          data = data,
-          col_id = if(nchar(input$in_sel_id) == 0) NULL else input$in_sel_id,
-          col_sort = input$in_sel_sort_col,
-          order_matches_sort = input$in_order_matches_sort,
-          maxlevels = input$in_maxlevels,
-          verbose = 1,
-          drop_unused_id_levels = FALSE,
-          palettes = NULL,
-          sort_type = input$in_sel_sort_type,
-          desc = input$in_desc,
-          limit_plots = TRUE,
-          max_plottable_cols = input$in_max_plottable_cols,
-          cols_to_plot = NULL,
-          tooltip_column_suffix = "_tooltip",
-          ignore_column_regex = if(nchar(input$in_ignore_regex) > 0) input$in_ignore_regex else NULL,
-          convert_binary_numeric_to_factor = input$in_convert_binary_numeric,
-          options = opts
-        )
+        if(input$in_radio_plot_type == "gg1d"){
+          gg1d(
+            data = data,
+            col_id = if(nchar(input$in_sel_id) == 0) NULL else input$in_sel_id,
+            col_sort = input$in_sel_sort_col,
+            order_matches_sort = input$in_order_matches_sort,
+            maxlevels = input$in_maxlevels,
+            verbose = 1,
+            drop_unused_id_levels = FALSE,
+            palettes = NULL,
+            sort_type = input$in_sel_sort_type,
+            desc = input$in_desc,
+            limit_plots = TRUE,
+            max_plottable_cols = input$in_max_plottable_cols,
+            cols_to_plot = NULL,
+            tooltip_column_suffix = "_tooltip",
+            ignore_column_regex = if(nchar(input$in_ignore_regex) > 0) input$in_ignore_regex else NULL,
+            convert_binary_numeric_to_factor = input$in_convert_binary_numeric,
+            options = opts
+          )
+        }
+        else
+          ggparallel(
+            data = data,
+            order_columns_by = input$in_sel_pcp_order_columns,
+            order_observations_by = input$in_sel_pcp_order_observations_by,
+            col_colour = if(nchar(input$in_sel_pcp_col_colour) == 0) NULL else input$in_sel_pcp_col_colour,
+            highlight = if(nchar(input$in_sel_pcp_highlight) == 0) NULL else input$in_sel_pcp_highlight,
+            scaling = input$in_sel_pcp_scaling,
+            options = ggparallel_options(
+              interactive_svg_width = input$in_pcp_svg_width,
+              interactive_svg_height =  input$in_pcp_svg_height
+              )
+          )
       },
       error = function(err){
         shinyWidgets::sendSweetAlert(session = session, title = "Failed to Generate GG1D Plot", text =  err2html(err))
